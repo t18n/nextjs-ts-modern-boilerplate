48facb0e7e6c66eea8d11bb90bed947c44dce670
Turbo <turbothinh@gmail.com>
2019-12-31 18:32:06 +0100

    Enable export static pages

---
+++ next.config.js
@@ -0,0 +1,31 @@
+
+const fetch = require('isomorphic-unfetch');
+
+const generatePages = async () => {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=x-men');
+  const data = await res.json();
+  const pages = data.map(entry => entry.show);
+
+  return pages;
+};
+
+module.exports = {
+  exportTrailingSlash: true,
+
+  // 
+  exportPathMap: async () => {
+    const paths = {
+      '/': { page: '/' },
+      '/about': { page: '/about' }
+    };
+
+    const pages = await generatePages();
+
+    pages.forEach(page => {
+      paths[`/post/${page.id}`] = { page: '/post/[id]', query: { id: page.id } };
+    });
+
+    return paths;
+  }
+
+};
