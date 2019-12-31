ee048c72616119b6b4ef3b7425c99075c104c315
Turbo <turbothinh@gmail.com>
2019-12-31 18:04:44 +0100

    Fetch post data by id

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,13 +1,22 @@
-import { useRouter } from 'next/router';
+import fetch from 'isomorphic-unfetch';
 import Layout from '../../components/fractals/Layout';
 
-export default function Post() {
-  const router = useRouter();
-
+export default function Post(props) {
   return (
     <Layout>
-      <h1>{router.query.id}</h1>
-      <p>This is the blog post content.</p>
+      <h1>{props.post.name}</h1>
+      <p>{props.post.summary.replace(/<[/]?[pb]>/g, '')}</p>
+      <img src={props.post.image.medium} />
     </Layout>
   );
 }
+
+Post.getInitialProps = async function (context) {
+  const { id } = context.query;
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+  const show = await res.json();
+
+  console.log(`Fetched show: ${show.name}`);
+
+  return { post: show };
+};
